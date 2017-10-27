dccefdb156898393bdb4f7769f051f17dddb3c92
Jenny Miderkvist <jenny.miderkvist@gmail.com>
2017-10-27 14:39:32 +0200

    first commit

---
+++ 2610172.API/2610172.API/Controllers/BookingController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Filters;
+using U241017.DATA;
+
+namespace _2610172.API.Controllers
+{
+    public class AllowCrossSiteJsonAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Response != null)
+                actionExecutedContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            base.OnActionExecuted(actionExecutedContext);
+        }
+    }
+
+    [AllowCrossSiteJson]
+
+    public class BookingController : ApiController
+    {
+        Repository _repo = Repository.GetRepo;
+
+        [HttpGet]
+        public List<Class1>GetBookings()
+        {
+            return _repo.Products;
+        }
+
+        [HttpGet]
+        public Class1 GetBookings(int id)
+        {
+            Class1 p = _repo.Products.Find(x => x.Id == id);
+            return p;
+        }
+
+        [HttpPost]
+        public void AddBooking(Class1 p)
+        {
+            _repo.Products.Add(p);
+
+        }
+
+        [HttpDelete]
+        public void RemoveProduct(int id)
+        {
+            Class1 p = _repo.Products.Find(x => x.Id == id);
+            _repo.Products.Remove(p);
+        }
+    }
+}
---
+++ 2610172.API/2610172.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2610172.API.WebApiApplication" Language="C#" %>
